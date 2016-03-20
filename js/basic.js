17b6a290974cf3697a98978d75b5d3c3e1c6cb5f
digi-reaper <abhishek@digifutura.com>
2016-03-20 14:15:43 +0530

    added everything

--- js/basic.js
+++ js/basic.js
@@ -1,10 +1,16 @@
 var manager = [];
 var rightBarShow = false;
+var profileManager = {cover:'none'};
 var currentTaskList = 0 ;
 function addTaskList () {
 	var topic  = document.getElementById('newTaskListTopic').value;
-	if ( topic === '' )
+	if ( topic.trim() === '' ){
+		document.getElementById ( 'tasklist-error-msg' ).innerHTML = 'can you please type something ? ';
 		return;
+	} else if ( topic.length > 200 ){
+		document.getElementById ( 'tasklist-error-msg' ).innerHTML = 'Oops !!! being verbose , try to cut it in short ';
+		return;
+	}
 	var taskList = {};
 	taskList.topic = topic;
 	taskList.tasks = [];
@@ -12,20 +18,31 @@ function addTaskList () {
 	localStorage.setItem ( 'taskManager' , JSON.stringify(manager) );
 	var pos = manager.length - 1;
 	createTaskListView( pos , topic );
+	currentTaskList = pos;
+	retrieveTask ( pos );
 }
 
 function addTask () {
 	var content = document.getElementById('newTaskContent').value;
 	var pos = currentTaskList;
-	if ( content === '' )
+	var msgBox = document.getElementById ( 'task-error-msg' );
+	if ( content.trim() === '' ){
+		msgBox.innerHTML = 'can you please type something ? ';
 		return;
+	} else if ( content.length > 200 ){
+		msgBox.innerHTML = 'Oops !!! being verbose , try to keep it short ';
+		return;
+	} else {
+		msgBox.innerHTML = ' you added one task , you can also edit , delete or mark it done ';
+	}
+
 	var task = {};
 	task.topic = content;
 	task.isDone = false;
 	manager[pos].tasks.push ( task );
 	localStorage.setItem ( 'taskManager' , JSON.stringify(manager) );
 	var taskIndex = manager[pos].tasks.length - 1;
-	createTaskGridView ( pos , taskIndex , content );
+	createTaskGridView ( pos , taskIndex , content , task.isDone );
 }
 
 function retrieveTaskList () {
@@ -34,18 +51,29 @@ function retrieveTaskList () {
 	}
 	if ( manager.length )
 		retrieveTask ( manager.length - 1 );
+	else
+		retrieveTask ( 0 );
 }
 
 function retrieveTask (pos) {
 	var heading = document.getElementById ( 'taskList-topic-selected' );
 	var taskGenerator = document.getElementById ( 'new-task-generator' );
 	var layout = document.getElementById ( 'task-container' );
+	if ( manager.length > 0 ) {
+		heading.innerHTML = manager[currentTaskList].topic;
+		taskGenerator.style.display = 'inline-block';
+	} else {
+		heading.innerHTML = 'NO TASK LIST ADDED ';
+		taskGenerator.style.display = 'none';
+		return;
+	}
+	heading.innerHTML = manager[pos].topic;
 	layout.innerHTML = '';
 	layout.appendChild(heading);
 	layout.appendChild(taskGenerator);
 	var taskList = manager[pos].tasks;
 	for (var i = 0; i < taskList.length; i++) {
-		createTaskGridView ( pos , i , taskList[i].topic);
+		createTaskGridView ( pos , i , taskList[i].topic , taskList[i].isDone );
 	}
 }
 
@@ -75,16 +103,16 @@ function createTaskListView (pos,topic) {
 	// create edit button
 	var edit = document.createElement ( 'i' );
 	edit.setAttribute ( 'task-action' , 'edit' );
-	edit.setAttribute ( 'class' , 'fa fa-pencil-square-o edit' );
+	edit.setAttribute ( 'class' , 'fa fa-pencil edit' );
 	
 	// create delete button
 	var del = document.createElement ( 'i' );
-	del.setAttribute ( 'class' , 'fa fa-minus-square del' );
+	del.setAttribute ( 'class' , 'fa fa-trash-o del' );
 	del.setAttribute ( 'task-action' , 'delete' );
 
 	// putting all edit del button inside toolbar
-	toolbar.appendChild( edit );
 	toolbar.appendChild( del );
+	toolbar.appendChild( edit );
 	
 	// putting the toolbar inside the tasklist container
 	cardElement.appendChild ( toolbar );
@@ -92,24 +120,25 @@ function createTaskListView (pos,topic) {
 	// here we get our card , putting all the pieces together
 	var taskListContainer = document.getElementById ( 'task-list-container' );
 	taskListContainer.insertBefore ( cardElement , taskListContainer.childNodes[0] );
+
+	activateTaskList ( cardElement );
 }
 
-function createTaskGridView (pos,taskIndex,task) {
+function createTaskGridView ( pos , taskIndex , task , isDone) {
 	// creating the task cotainer -- will be full card to be shown as task
 	var taskGrid = document.createElement ('section');
 	taskGrid.setAttribute ( 'class' , 'task' );
-	taskGrid.setAttribute ( 'id' , 'list-' + pos + '-task-' + taskIndex );
-
+	if ( isDone )
+		taskGrid.setAttribute( 'class' , 'task task-done')
+	
 	// creating the task content container -- will be child of task container
 	var taskContent = document.createElement( 'div' );
 	taskContent.setAttribute ( 'class' , 'task-content' );
-	taskContent.setAttribute ( 'id' , 'list-' + pos + '-task-content-' + taskIndex );
-
+	
 	// creating the task toolbar to delete or edit or mark as done -- will be child of task container
 	var toolbar = document.createElement( 'div' );
 	toolbar.setAttribute ( 'class' , 'task-toolbar' );
-	toolbar.setAttribute ( 'id' , 'list-' + pos + '-toolbar-' + pos );
-
+	
 	// creating the text node containing the task
 	var content = document.createTextNode ( task );
 
@@ -119,23 +148,26 @@ function createTaskGridView (pos,taskIndex,task) {
 	// create done button
 	var done = document.createElement ( 'i' );
 	done.setAttribute ( 'class' , 'fa fa-check-circle-o done' );
-	done.setAttribute ( 'id' , 'list-' + pos + '-done-' + taskIndex );
+	if ( isDone )
+		done.setAttribute( 'class' , 'fa fa-check-circle done' );
+	done.setAttribute ( 'task-action' , 'done' );
 
 	// create edit button
 	var edit = document.createElement ( 'i' );
-	edit.setAttribute ( 'class' , 'fa fa-pencil-square-o edit' );
-	edit.setAttribute ( 'id' , 'list-' + pos + '-edit-' + taskIndex );
+	edit.setAttribute ( 'class' , 'fa fa-pencil edit' );
+	edit.setAttribute ( 'task-action' , 'edit' );
 
 	// create delete button
 	var del = document.createElement ( 'i' );
-	del.setAttribute ( 'class' , 'fa fa-minus-square del' );
-	del.setAttribute ( 'id' , 'list-' + pos + '-del-' + taskIndex );
+	del.setAttribute ( 'class' , 'fa fa-trash-o del' );
+	del.setAttribute ( 'task-action' , 'delete' );
+
 
 	// putting all edit del done button inside toolbar
 	toolbar.appendChild( done );
-	toolbar.appendChild( edit );
 	toolbar.appendChild( del );
-
+	toolbar.appendChild( edit );
+	
 
 	// putting the task tool bar in task container -- first child of task container
 	taskGrid.appendChild ( toolbar );
@@ -145,7 +177,7 @@ function createTaskGridView (pos,taskIndex,task) {
 
 	// put all the things together in the dashboard to show the different grid for different task of a list
 	var taskBoard = document.getElementById ( 'task-container' );
-	taskBoard.insertBefore ( taskGrid , taskBoard.childNodes[4] );
+	taskBoard.insertBefore ( taskGrid , taskBoard.childNodes[2] );
 }
 
 
@@ -167,11 +199,47 @@ function getCustomiser () {
 	}
 }
 
+function activateTaskList ( child ) {
+	var	childrenArray = child.parentNode.children ;
+	for (var i = 0; i < childrenArray.length; i++) {
+		childrenArray[i].style.boxShadow = 'none';
+	}
+	child.style.boxShadow = '1px 10px 10px 1px black';
 
+}
+
+function modalOpen ( id , topicIndex , taskIndex) {
+	console.log(id);
+	console.log(topicIndex);
+	console.log(taskIndex);
+	if ( id === 'modal-topic' ) {
+		document.getElementById( 'modal-task' ).style.display = 'none';
+		document.getElementById( 'tasklist-editor' ).value = manager[topicIndex].topic;
+		
+	} else if ( id === 'modal-task' ) {
+		document.getElementById( 'modal-topic' ).style.display = 'none';
+		document.getElementById( 'task-editor' ).value = manager[topicIndex].tasks[taskIndex].topic;
+	}
+	document.getElementById( id ).style.display = 'block';
+}
+
+function modalClose ( ) {
+	document.getElementById( 'modal-task' ).style.display = 'none';
+	document.getElementById( 'modal-topic' ).style.display = 'none';
+	document.getElementById( 'task-editor' ).value = '';
+	document.getElementById( 'tasklist-editor' ).value = '';
+}
 
 window.onload = function () {
-	if( localStorage.getItem('taskManager') != null){
+
+	if( localStorage.getItem ( 'taskManager' ) != null){
 		manager = JSON.parse(localStorage.getItem('taskManager'));
+		currentTaskList = manager.length - 1;
+	}
+	if( localStorage.getItem ( 'taskManagerProfile' ) != null){
+		profileManager = JSON.parse ( localStorage.getItem ( 'taskManagerProfile' ) );
+		document.body.style.backgroundImage = profileManager.cover;
+			
 	}
 	retrieveTaskList();
 	getCustomiser();
@@ -182,6 +250,8 @@ window.onload = function () {
 		if(event.srcElement.getAttribute('class') === "back-cover") {
 			var target = event.srcElement; 
 			document.body.style.backgroundImage = target.style.backgroundImage;
+			profileManager.cover = target.style.backgroundImage;
+			localStorage.setItem( 'taskManagerProfile' , JSON.stringify ( profileManager ) );
 		}
 	} 
 
@@ -203,28 +273,94 @@ window.onload = function () {
 		}	
 		if ( event.srcElement.hasAttribute ( 'task-action' ) ) {
 			var action = event.srcElement.getAttribute ( 'task-action' );
-			console.log(pos);
 			if ( action === 'delete') {
 				manager.splice ( pos , 1 );
 				localStorage.setItem ( 'taskManager' , JSON.stringify(manager) );
 				card.parentNode.removeChild ( card );
+				if ( pos == currentTaskList ) {
+					retrieveTask ( pos );
+				}
 			} else if ( action == 'edit') {
-				alert('edit');
+				modalOpen( 'modal-topic' , pos , null);
 			}
 		} else {
-			console.log(pos);
+			activateTaskList(card);
 			document.getElementById ( 'taskList-topic-selected' ).innerHTML = manager[pos].topic;
 			currentTaskList = pos;
 			retrieveTask ( pos );
 		}
 	}
 
-	// handling the change of topic 
-	document.getElementById( '' )
 	// event handling for tasks
 	var taskContainer = document.getElementById( 'task-container' );
 	taskContainer.onclick = function ( e ) {
+		var card = event.srcElement;
+		while( card !=null  && card.getAttribute ( 'class' ) !=  'task' && card.getAttribute ( 'class' ) !=  'task task-done' ){
+			var styleClass = card.getAttribute ( 'class' );
+			if ( card.getAttribute ( 'class' ) === 'dashboard')
+				return;
+			card = card.parentNode;
+		}
+		var taskListArray = card.parentNode.children;
+		var pos = 0;
+		for (var i = 0; i < taskListArray.length; i++) {
+			if( taskListArray[i] == card )
+				pos = taskListArray.length - 1 - i;
+		}	
+		if ( event.srcElement.hasAttribute ( 'task-action' ) ) {
+			var action = event.srcElement.getAttribute ( 'task-action' );
+			console.log(pos);
+			if ( action === 'delete') {
+
+				manager[currentTaskList].tasks.splice ( pos , 1 );
+				console.log(currentTaskList);
+				localStorage.setItem ( 'taskManager' , JSON.stringify(manager) );
+				card.parentNode.removeChild ( card );
+			} else if ( action == 'edit') {
+				modalOpen( 'modal-task' , currentTaskList , pos );
+			} else if ( action == 'done') {
+				if ( !manager[currentTaskList].tasks[pos].isDone ){
+					event.srcElement.setAttribute ( 'class' , 'fa fa-check-circle done');
+					card.setAttribute ( 'class' , 'task task-done');
+					manager[currentTaskList].tasks[pos].isDone = true;
+					localStorage.setItem ( 'taskManager' , JSON.stringify(manager) );
+				} else {
+					event.srcElement.setAttribute ( 'class' , 'fa fa-check-circle-o done');
+					card.setAttribute ( 'class' , 'task');
+					manager[currentTaskList].tasks[pos].isDone = false;
+					localStorage.setItem ( 'taskManager' , JSON.stringify(manager) );	
+				}
+			}
+		}
 	}
+
+	// event handler for modal close button
+	var closeTaskModal = document.getElementById('closeModal-task');
+	closeTaskModal.onclick = function ( e ) {
+		modalClose();
+	}
+	var closeTopicModal = document.getElementById('closeModal-topic');
+	closeTopicModal.onclick = function ( e ) {
+		modalClose();
+	}
+
+	//event for add task topic/tasklist button
+	document.getElementById( 'toggle-sidebar-button' ).onclick = function ( e ) {
+		toggleCustomiser()
+	} 
+
+	//event for add task topic/tasklist button
+	document.getElementById( 'add-tasklist-button' ).onclick = function ( e ) {
+		addTaskList();
+		document.getElementById( 'newTaskListTopic' ).value = '';	
+	} 
+
+	//event for add task topic/tasklist button
+	document.getElementById( 'add-task-button' ).onclick = function ( e ) {
+		addTask();
+		document.getElementById( 'newTaskContent' ).value = '';	
+	} 
+
 }
 
 
